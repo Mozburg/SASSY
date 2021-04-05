17266d8b2ea1d7db039a9df49144d50db08d6784
Lillithiun <Mozburger37@gmail.com>
2021-04-04 19:36:19 -0700

    replaced old lib with new lib

--- SensorsLibrary/rpm.cpp
+++ SensorsLibrary/rpm.cpp
@@ -8,15 +8,35 @@ RPM::RPM() {
 
 int RPM::GetRPM(void) {
 	float val = analogRead(A5);  //Value is high if reflective tape is infront of sensor
+//----------------------------------------------------------------------------------mozburg
+	//buffer vars
+	float max = 1;
+	float min = 0;
+	float mid = .5;
+	
+	//max set
+	max = val*(val >= max) + max*(val <= max);
+	
+	//min set
+	min = val*(val <= min) + min*(val >= min);
+	
+	//mid set
+        mid = (max + min) / 2;
 
-    if(val > 225.0) {
+	//1-0 mid check
+	return (val > mid); 
+//-------------------------------------------------------------------------------------
+}
+	
+	
+/*    if(val > mid) {
 		return 1; //Increases rotation each pulse
     }
 
 	else {
 		return 0;
 	}
-}
+
 /*
 inline void __attribute__ ((constructor)) initLibrary(void) {
  //
@@ -26,7 +46,7 @@ inline void __attribute__ ((constructor)) initLibrary(void) {
 }
 inline void __attribute__ ((destructor)) cleanUpLibrary(void) {
  //
- // Function that is called when the library is »closed«.
+ // Function that is called when the library is Â»closedÂ«.
  //
     printf("Library is exited\n"); 
 }*/
