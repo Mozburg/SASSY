370860a3c3d9754ec63b926b3e2f7bcd985f6ebe
Jonathan Maravilla <66583170+MaraCoding@users.noreply.github.com>
2021-04-22 08:22:33 -0700

    Add files via upload
    This is the working current sensor code. All other version are now outdated. This version does not require a library file.

---
+++ WORKING_DrillAndSystemCurrent.ino
@@ -0,0 +1,47 @@
+/*
+ * Code sourced by Alan, edited by Brandon Mosburg, property of the creators.
+int analogPin = A1; // potentiometer wiper (middle terminal) connected to analog pin 3
+ outside leads to ground and +5V 
+ */
+
+ 
+//==========================================================================================================
+
+float val1 = 512; // variable to store the value read
+float val2 = 512;
+float Vmap = 0; //Map to voltage
+float Vmap2 = 0;
+float drillAmps = 0; //Conversion to amps
+float systemAmps = 0;
+
+//==========================================================================================================
+void setup() {
+  TCCR0A=0;
+  TCCR0B=0;
+  TCNT0=0;
+  
+  OCR0A = 46;  //Top count is 16M/(1024*338), for 338Hz sampling at 1024 prescale value (rounded down).
+  TIMSK0 |= (1<<OCIE0A); //enable timer mask interrupts
+  TCCR0A |= (1<<WGM01); //CTC mode
+  TCCR0B |= (1<<CS02) | (1<<CS00); //Prescale value 1024
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT); // input init
+  Serial.begin(115200); // setup serial
+  //delay(1); // delay for arduino serial init
+}
+//=========================================================================================================
+void loop() {
+}
+
+
+ISR(TIMER0_COMPA_vect){
+    val1 = analogRead(A1); // read the input pin
+    Vmap = ((5 * val1 / 1024) - 2.5); //map analog val
+    drillAmps = Vmap * 10; //map amp val
+    Serial.println(drillAmps);
+    
+    val2 = analogRead(A2); // read the input pin
+    Vmap2 = ((5 * val2 / 1024) - 2.5); //map analog val
+    systemAmps = Vmap2 * 10; //map amp val
+    //Serial.println(systemAmps);
+}
